6fab34ff0dd139424f2644a264847d619bed7026
Nelih80 <88829865+Nelih80@users.noreply.github.com>
2024-06-26 15:19:33 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -57,10 +57,10 @@ res1() {
 netfilter-persistent
 clear
 echo -e "\e[92;1m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \033[0;36m            UPDATE SCRIPT            \e[0m"
+echo -e " \033[46;0;37m          UPDATE SCRIPT            \e[0m"
 echo -e "\e[92;1m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-echo -e "  \033[0;36m update script service\e[92;1m"
+echo -e "  \033[0;32m update script service\e[92;1m"
 fun_bar 'res1'
 echo -e "\e[92;1m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
